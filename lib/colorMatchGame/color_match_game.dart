8a729ed44a8b1bb2fe3482fd7cbbc33ef1110de7
yuhan <yw10588@gmail.com>
2023-03-07 14:06:12 +0800

    產生三個難度等級的隨機色

--- lib/colorMatchGame/color_match_game.dart
+++ lib/colorMatchGame/color_match_game.dart
@@ -17,7 +17,7 @@ class _ColorMatchGameState extends State<ColorMatchGame> {
 
   @override
   void initState(){
-    colorList = initColor(30);
+    colorList = initColor(Level.hard,24).shuffled();
     super.initState();
   }
 
@@ -45,28 +45,63 @@ class _ColorMatchGameState extends State<ColorMatchGame> {
     );
   }
 
-  List<Color> initColor(int count){
+  List<Color> initColor(Level level,int count){
     List<Color> list = [];
-    int rColor = random.nextInt(100);
-    int gColor = random.nextInt(100);
-    int bColor = random.nextInt(100);
-    var mColor = [rColor,gColor,bColor].reduce(min);
-    var rGap = rColor - mColor;
-    var gGap = gColor - mColor;
-    var bGap = bColor - mColor;
-    while(list.length < count){
-      bool reset = rColor + 25 > 255 || gColor + 25 > 255 || bColor + 25 > 255;
-      rColor = reset ?  rGap : rColor + 25;
-      gColor = reset ?  gGap : gColor + 25;
-      bColor = reset ?  bGap : bColor + 25;
-      var o = random.nextDouble().toStringAsFixed(2).toDouble();
-      var tempo = o < 0.3 ? 0.3 : o;
-      Color tempCol = Color.fromRGBO(rColor, gColor, bColor, tempo);
-      if(!list.contains(tempCol)){
-        AppLog('$rColor/$gColor/$bColor/$tempo');
-        list.add(tempCol);
-      }
+    late int rColor;
+    late int gColor;
+    late int bColor;
+    switch(level){
+      case Level.easy:
+        while(list.length < count){
+          rColor = random.nextInt(256);
+          gColor = random.nextInt(256);
+          bColor = random.nextInt(256);
+          Color tempCol = Color.fromRGBO(rColor, gColor, bColor, 1);
+          if(!list.contains(tempCol)){
+            list.add(tempCol);
+          }
+        }
+        break;
+      case Level.medium:
+        List<int> cList = [random.nextInt(256),random.nextInt(256),random.nextInt(256)];
+        int mainC = random.nextInt(3);
+        while(list.length < count){
+          var colors = List.generate(3, (index) => index == mainC ? cList[mainC] : random.nextInt(256));
+          Color tempCol = Color.fromRGBO(colors[0], colors[1], colors[2], 1);
+          if(!list.contains(tempCol)){
+            AppLog('${colors[0]}/${colors[1]}/${colors[2]}');
+            list.add(tempCol);
+          }
+        }
+        break;
+      case Level.hard:
+        rColor = random.nextInt(100);
+        gColor = random.nextInt(100);
+        bColor = random.nextInt(100);
+        var mColor = [rColor,gColor,bColor].reduce(min);
+        var rGap = rColor - mColor;
+        var gGap = gColor - mColor;
+        var bGap = bColor - mColor;
+        while(list.length < count){
+          bool reset = rColor + 25 > 255 || gColor + 25 > 255 || bColor + 25 > 255;
+          rColor = reset ? rGap : rColor + 25;
+          gColor = reset ? gGap : gColor + 25;
+          bColor = reset ? bGap : bColor + 25;
+          var o = random.nextDouble().toStringAsFixed(2).toDouble();
+          var tempo = o < 0.3 ? 0.3 : o;
+          Color tempCol = Color.fromRGBO(rColor, gColor, bColor, tempo);
+          if(!list.contains(tempCol)){
+            // AppLog('$rColor/$gColor/$bColor/$tempo');
+            list.add(tempCol);
+          }
+        }
     }
-    return list.shuffled();
+    return list;
   }
 }
+
+
+enum Level{
+ easy, medium, hard
+}
+
